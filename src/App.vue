8d243615503768616a317d3d9ead14b2d4122c4c
Sajith K <sajithmenon93@gmail.com>
2023-09-14 20:18:50 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HomeView from "./views/HomeView.vue";
+</script>
+
+<template>
+  <home-view></home-view>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Datepicker from "vue3-datepicker";
+
+interface Account {
+  title: string;
+  accountBalance: number;
+  minimumBalance?: number;
+}
+
+const accounts = ref<Account[]>([]);
+const accountTitle = ref<string>("");
+const accountBalance = ref<number>(0);
+const minimumBalance = ref<number>(0);
+
+const addAccount = () => {
+  accounts.value.push({
+    title: accountTitle.value,
+    accountBalance: accountBalance.value,
+    minimumBalance: minimumBalance.value,
+  });
+  accountTitle.value = "";
+  accountBalance.value = 0;
+  minimumBalance.value = 0;
+};
+</script>
+
+  <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>Zazu</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <h5>Accounts</h5>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Account Title"
+          v-model="accountTitle"
+        />
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Account Balance"
+          v-model="accountBalance"
+        />
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Minimum Balance"
+          v-model="minimumBalance"
+        />
+        <datepicker />
+        <button class="btn btn-primary" @click="addAccount">Add Account</button>
+        <div v-for="account in accounts" v-bind:key="account.title">
+          <h6>{{ account.title }}</h6>
+          <p>Balance: {{ account.accountBalance }}</p>
+          <p>Minimum Balance: {{ account.minimumBalance }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
