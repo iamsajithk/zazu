81043b9d5f321202f93a04f0695a841a3af23048
Sajith K <sajithmenon93@gmail.com>
2023-09-16 14:31:50 +0530

    Testing auto deploy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -109,7 +109,7 @@ const addAccount = () => {
               <input
                 type="text"
                 class="form-control"
-                placeholder="Account Title"
+                placeholder="Income Title"
                 v-model="accountTitle"
               />
             </div>
